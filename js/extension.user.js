4edf815ff510d6ae05a546524c4cafe281633164
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-08-23 14:38:18 -0300

    O site detecta a extensão instalada

--- js/extension.user.js
+++ js/extension.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Escutar mensagens do Google Meet
-// @version      0.1
+// @version      0.2
 // @description  Extensão que adiciona um recurso de falar em voz alta as novas mensagens no Google Meet
 // @author       Jefferson Dantas
 // @homepage     https://josejefferson.github.io/speak-meet-messages/
 // @supportURL   https://github.com/josejefferson/speak-meet-messages/issues
 // @updateURL    https://josejefferson.github.io/speak-meet-messages/js/extension.user.js
 // @downloadURL  https://josejefferson.github.io/speak-meet-messages/js/extension.user.js
-// @match        https://meet.google.com/*
+// @include      https://meet.google.com/*
+// @include      https://josejefferson.github.io/speak-meet-messages/
+// @include      http://localhost:8080/
 // @icon         https://www.google.com/s2/favicons?domain=meet.google.com
 // @grant        none
 // ==/UserScript==
@@ -19,8 +21,8 @@
 // ┃ "false" significa "Não"         ┃
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-// Diz alguma expressão antes de cada mensagem, por exemplo: "Nova mensagem!"
-const Texto_para_falar_antes_da_mensagem = ""
+// Diz alguma expressão antes de cada mensagem, por exemplo: 'Nova mensagem!'
+const Texto_para_falar_antes_da_mensagem = ''
 
 // Se true, fala o nome da pessoa que enviou a mensagem
 const Falar_nome = true
@@ -166,16 +168,195 @@ const Velocidade_da_fala = 1.2
 
 
 // NÃO MEXER ABAIXO
-window.speakMessagesOptions = {
-	beforeText: Texto_para_falar_antes_da_mensagem, //
-	speakName: Falar_nome, //
-	fullName: Falar_nome_completo,//
-	nameAfter: Falar_nome_depois_da_mensagem,//
+const options = {
+	beforeText: Texto_para_falar_antes_da_mensagem,
+	speakName: Falar_nome,
+	fullName: Falar_nome_completo,
+	nameAfter: Falar_nome_depois_da_mensagem,
 	meetOpen: Falar_com_a_tela_do_Meet_aberta,
 	interruptPrev: Interromper_mensagem_anterior,
-	voiceSpeed: Velocidade_da_fala//
+	voiceSpeed: Velocidade_da_fala
 }
 
-fetch('https://josejefferson.github.io/speak-meet-messages/js/extension.js')
-	.then(r => r.text())
-	.then(r => eval(r))
+let active = localStorage.getItem('speak-messages.active') === 'true'
+const $button = document.createElement('button')
+
+const selectors = {
+	participantId: '[data-initial-participant-id]',
+	beforeButton: '.iJq2Ce',
+	msgBubble: '.NSvDmb',
+	sender: '.UgDTGe',
+	message: '.xtO4Tc'
+}
+
+const $css = document.createElement('style')
+$css.innerText = `
+	.speak-messages {
+		align-items: center;
+		background-color: #ea4335;
+		border-radius: 50%;
+		border: none;
+		color: #fff;
+		cursor: pointer;
+		display: inline-flex;
+		height: 40px;
+		justify-content: center;
+		margin: 0 6px;
+		user-select: none;
+		vertical-align: middle;
+		width: 40px;
+	}
+
+	.speak-messages .google-material-icons::before {
+		content: 'volume_off';
+	}
+
+	.speak-messages.active {
+		background-color: #3c4043;
+	}
+
+	.speak-messages.active .google-material-icons::before {
+		content: 'volume_up';
+	}
+
+	.speak-messages:hover {
+		box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 1px 3px 1px rgb(60 64 67 / 15%);
+	}
+
+	.speak-messages:disabled {
+		color: rgba(232, 234, 237, 0.38);
+		opacity: .25;
+		pointer-events: none;
+	}
+`
+
+tryStart()
+function tryStart() {
+	if (document.querySelector(selectors.participantId)) {
+		start()
+	}
+	else {
+		setTimeout(tryStart, 1000)
+	}
+}
+
+function start() {
+	// Configura o botão
+	$button.innerHTML += '<i class="google-material-icons"></i>'
+	$button.title = 'Ativar/desativar mensagens em voz alta'
+	$button.classList.add('speak-messages')
+
+	// Insere o botão de ativar/desativar
+	const $before = document.querySelector(selectors.beforeButton)
+	if ($before) {
+		$before.parentNode.insertBefore($button, $before.nextElementSibling)
+		$before.parentNode.insertBefore($css, $before.nextElementSibling)
+	}
+
+	// Voz não suportada
+	if (!('speechSynthesis' in window)) {
+		$button.disabled = true
+		$button.title = 'Seu dispositivo não suporta mensagens em voz alta'
+		return
+	}
+
+	getVoice(voice => {
+		// Não há voz disponível
+		if (!voice) {
+			$button.disabled = true
+			$button.title = 'Seu dispositivo não suporta mensagens em voz alta'
+			return
+		}
+
+		// Configura o botão
+		if (active) $button.classList.add('active')
+		$button.addEventListener('click', toggle)
+
+		watchMessages(voice)
+	})
+}
+
+
+// Retorna voz disponível
+function getVoice(callback) {
+	speechSynthesis
+	setTimeout(() => {
+		const voices = speechSynthesis.getVoices()
+		let voice = voices.find(voice => {
+			return voice.name === 'Google português do Brasil'
+		}) || voices.find(voice => {
+			return voice.lang.toLowerCase().includes('pt-br')
+		})
+
+		callback(voice)
+	}, 2000)
+}
+
+
+// Ativa/desativa a voz
+function toggle() {
+	active = !active
+	$button.classList[active ? 'add' : 'remove']('active')
+	localStorage.setItem('speak-messages.active', active)
+	stopSpeak()
+}
+
+
+// Aguarda por mensagens
+function watchMessages(voice) {
+	const observer = new MutationObserver((mutationRecord) => {
+		const messageElement = mutationRecord[mutationRecord.length - 1].addedNodes[0]
+
+		if (messageElement && active && (options.meetOpen || document.hidden)) {
+			let sender = messageElement.querySelector(selectors.sender).innerText
+			sender = options.fullName ? sender : sender.split(' ').slice(0, 2).join(' ')
+
+			const message = messageElement.querySelector(selectors.message).innerText
+
+			let phrase = options.beforeText + ' '
+
+			if (options.nameAfter) {
+				phrase += message
+				if (options.speakName) phrase += ', disse ' + sender
+			}
+			else {
+				if (options.speakName) phrase += sender + 'disse, '
+				phrase += message
+			}
+
+			speak(phrase, options.voiceSpeed || 1.2, voice)
+		}
+	})
+
+	const $msgBubble = document.querySelector(selectors.msgBubble)
+	observer.observe($msgBubble, { childList: true, subtree: true })
+}
+
+
+// Fala a mensagem
+function speak(message, speed, voice) {
+	if (options.interruptPrev) stopSpeak()
+
+	const utterance = new SpeechSynthesisUtterance()
+	utterance.text = message
+	utterance.lang = 'pt-BR'
+	utterance.voice = voice
+	utterance.rate = speed
+
+	speechSynthesis.speak(utterance)
+}
+
+
+// Para de falar
+function stopSpeak() {
+	speechSynthesis.cancel()
+}
+
+console.log(
+	'%c🎤 Escutar mensagens do Meet%cv' + GM_info.script.version,
+	'background-color:#06f;border-radius:99px;color:#fff;font-family:Roboto,sans-serif;margin-right:5px;padding:5px 10px',
+	'background-color:#f71;border-radius:99px;color:#fff;font-family:Roboto,Arial,sans-serif;padding:5px 10px'
+)
+
+window.speakMeetMessagesInstalled = true
+window.speakMeetMessagesVersion = GM_info.script.version
--- js/index.js
+++ js/index.js
@@ -1,7 +1,74 @@
+const CURRENT_VERSION = '0.2'
+
+const LS_WAIT_INSTALL = 'speakMeetMessages.waitInstall'
+const LS_LAST_VERSION = 'speakMeetMessages.lastVersion'
+
 const $chrome = document.querySelectorAll('.chrome')
 const $firefox = document.querySelectorAll('.firefox')
+const $install = document.querySelectorAll('.install')
+const $installed = document.querySelector('.installed')
+const $updateAvailable = document.querySelector('.update-available')
+const $version = document.querySelectorAll('.version')
+const $newVersion = document.querySelectorAll('.new-version')
 
 if (navigator.userAgent.includes('Firefox')) {
 	$chrome.forEach(e => e.classList.add('hidden'))
 	$firefox.forEach(e => e.classList.remove('hidden'))
 }
+
+$install.forEach(e => e.addEventListener('click', () => {
+	console.log('click')
+	document.addEventListener('visibilitychange', () => {
+		console.log(document.hidden)
+		if (!document.hidden) {
+			// Detecta quando a extensão é instalada
+			localStorage.setItem(LS_WAIT_INSTALL, true)
+			const version = window.speakMeetMessagesVersion
+			if (version) {
+				localStorage.setItem(LS_LAST_VERSION, version)
+			}
+			location.reload()
+		}
+	})
+}))
+
+window.addEventListener('load', () => {
+	setTimeout(() => {
+		const installed = window.speakMeetMessagesInstalled
+		const version = window.speakMeetMessagesVersion
+	
+		if (installed) {
+			// Extensão instalada
+			$version.forEach(e => e.innerText = version)
+			$install[0].classList.add('btn-warning')
+			$install[0].querySelector('span').innerText = 'Reinstalar extensão'
+			$installed.classList.remove('hidden')
+			
+			if (version !== CURRENT_VERSION) {
+				// Nova atualização disponível
+				$installed.classList.add('hidden')
+				$install[0].classList.add('btn-danger')
+				$install[0].querySelector('span').innerText = 'Atualizar extensão'
+				$newVersion.forEach(e => e.innerText = CURRENT_VERSION)
+				$updateAvailable.classList.remove('hidden')
+			}
+	
+		}
+	
+		if (localStorage.getItem(LS_WAIT_INSTALL)) {
+			if (installed) {
+				// Sucesso na instalação
+				localStorage.setItem(LS_LAST_VERSION, version)
+				Swal.fire({
+					toast: true,
+					position: 'top-end',
+					showConfirmButton: false,
+					timer: 5000,
+					icon: 'success',
+					text: 'Instalação concluída',
+				})
+			}
+			localStorage.removeItem(LS_WAIT_INSTALL)
+		}
+	}, 100)
+})
